262ed2c9e58319623551ad48dcebbc13005e8b5f
seonghun <tjdgns8439@naver.com>
2022-11-01 18:00:49 +0900

    chdir()
    cd 구현

--- MyShell.c
+++ MyShell.c
@@ -2,6 +2,7 @@
 #include <unistd.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
 #define MAX_CMD_ARG 10
 #define BUFSIZ 256
 
@@ -36,19 +37,37 @@ int makelist(char *s, const char *delimiters, char** list, int MAX_LIST){
   return numtokens;
 }
 
-int main(int argc, char**argv){
+void myShell_cd( int argc, char **argv ){
+	char* path;
+	
+	if (argc == 1){
+		path = getenv("HOME");
+	}
+	else{
+		path = argv[1];
+	}
+	if (chdir(path)!=0)
+		perror("error: ");
+}
+
+int main(int argc, char **argv){
   int i=0;
   pid_t pid;
   while (1) {
   	fputs(prompt, stdout);
 	fgets(cmdline, BUFSIZ, stdin);
 	cmdline[strlen(cmdline) -1] = '\0';
+	int cargc = makelist(cmdline, " \t", cmdvector, MAX_CMD_ARG);
+
 	if (strcmp("exit", cmdline) == 0)
 		break;
+
+	if (strcmp("cd", cmdvector[0]) == 0){
+		myShell_cd(cargc, cmdvector);
+	}
   
 	switch(pid=fork()){
 	case 0:
-		makelist(cmdline, " \t", cmdvector, MAX_CMD_ARG);
 		execvp(cmdvector[0], cmdvector);
 		fatal("main()");
 	case -1:
