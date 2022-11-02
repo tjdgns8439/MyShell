5e9964ad0c6171782e3d0c65c5874fd5cf43adc4
seonghun <tjdgns8439@naver.com>
2022-11-02 18:19:07 +0900

    zombie process
    백그라운드 실행, 좀피프로세스 생성

--- MyShell.c
+++ MyShell.c
@@ -46,8 +46,20 @@ void myShell_cd( int argc, char **argv ){
 	else{
 		path = argv[1];
 	}
-	if (chdir(path)!=0)
+	if (chdir(path)<0)
 		perror("error: ");
+}
+int is_background(char *cmdline)
+{    
+	for(int i=0; i < strlen(cmdline); i++)
+	{
+        	if(cmdline[i] == '&')
+        	{
+            		cmdline[i] = ' ';
+           		return 1;
+        	}
+	}
+	return 0;
 }
 
 int main(int argc, char **argv){
@@ -56,23 +68,28 @@ int main(int argc, char **argv){
   while (1) {
   	fputs(prompt, stdout);
 	fgets(cmdline, BUFSIZ, stdin);
+	int bg = is_background(cmdline);
+
 	cmdline[strlen(cmdline) -1] = '\0';
 	int cargc = makelist(cmdline, " \t", cmdvector, MAX_CMD_ARG);
-
 	if (strcmp("exit", cmdline) == 0)
+
 		break;
 
 	if (strcmp("cd", cmdvector[0]) == 0){
 		myShell_cd(cargc, cmdvector);
+		continue;
 	}
-  
+
+ 
 	switch(pid=fork()){
 	case 0:
-		execvp(cmdvector[0], cmdvector);
-		fatal("main()");
+		execvp(cmdvector[0], cmdvector);//자식이 먼저 죽음.
+		fatal("main()");//execvp 함수에 대해서 알아보기.
 	case -1:
   		fatal("main()");
 	default:
+		if(bg==1) break;
 		wait(NULL);
 	}
   }
