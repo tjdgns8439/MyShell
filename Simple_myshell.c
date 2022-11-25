0f0bde22de5ec9532522c1b61cbfdaaa4fd25cad
seonghun <tjdgns8439@naver.com>
2022-11-25 20:25:15 +0900

    signal처리
    - ^c ^z ^\ 무시.
    - 좀비프로세스 생성방지

--- Simple_myshell.c
+++ Simple_myshell.c
@@ -3,6 +3,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <sys/wait.h>
+#include <signal.h>
 #define MAX_CMD_ARG 10
 #define BUFSIZ 256
 
@@ -40,7 +42,7 @@ int makelist(char *s, const char *delimiters, char** list, int MAX_LIST){
 void myShell_cd( int argc, char **argv ){
 	char* path;
 	
-	if (argc == 1){
+	if (argc == 1 || strcmp(argv[1], "~")==0){
 		path = getenv("HOME");
 	}
 	else{
@@ -62,34 +64,56 @@ int is_background(char *cmdline)
 	return 0;
 }
 
+static void child_handler(int signo){
+	int status;
+	int pid;
+	while((pid = waitpid(-1, &status, WNOHANG))>0){
+	}
+}
+
+void sig_handler(int signo){
+	printf("\n");
+}
+
 int main(int argc, char **argv){
   int i=0;
   pid_t pid;
+ 	signal(SIGCHLD, (void*)child_handler);
+	signal(SIGINT, SIG_IGN);
+        signal(SIGTSTP, SIG_IGN);
+        signal(SIGQUIT, SIG_IGN);
   while (1) {
   	fputs(prompt, stdout);
 	fgets(cmdline, BUFSIZ, stdin);
-	int bg = is_background(cmdline);
+	int back = is_background(cmdline);
 
 	cmdline[strlen(cmdline) -1] = '\0';
 	int cargc = makelist(cmdline, " \t", cmdvector, MAX_CMD_ARG);
-	if (strcmp("exit", cmdline) == 0)
-
-		break;
+	if (strcmp("exit", cmdline) == 0) break;
 
 	if (strcmp("cd", cmdvector[0]) == 0){
 		myShell_cd(cargc, cmdvector);
 		continue;
 	}
-
- 
+	
 	switch(pid=fork()){
 	case 0:
-		execvp(cmdvector[0], cmdvector);//자식이 먼저 죽음.
-		fatal("main()");//execvp 함수에 대해서 알아보기.
+	if(back!=1){
+		signal(SIGINT, SIG_DFL);
+		signal(SIGTSTP, SIG_DFL);
+		signal(SIGQUIT, SIG_DFL);
+		}
+	else{
+		signal(SIGINT, SIG_IGN);
+        	signal(SIGTSTP, SIG_IGN);
+        	signal(SIGQUIT, SIG_IGN);
+	}
+		execvp(cmdvector[0], cmdvector);
+		fatal("main()");
 	case -1:
   		fatal("main()");
 	default:
-		if(bg==1) break;
+		if(back == 1) break;
 		wait(NULL);
 	}
   }
